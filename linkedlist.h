8df9f7c8b40827738aa52212c81c3650f33fc0af
ishmum123 <ishmumkhan@yahoo.co.in>
2017-04-26 14:32:12 +0600

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -8,214 +8,118 @@ using std::ostream;
 #define Ptr Node<Data>
 
 template<class Data>
-struct Node
-{
+struct Node {
+    Node(Data data, Ptr *next) : data(data), next(next) {}
     Data data;
     Ptr *next;
 };
 
 template<class Data>
-class linkedlist
-{
+class linkedlist {
 private:
     Ptr *head;
 
 public:
-    linkedlist()
-    {
-        head = NULL;
-    }
-
-    ~linkedlist()
-    {
-        if(head != NULL)
-        {
-            Ptr *curr = head;
-            Ptr *next;
-
-            while(curr != NULL)
-            {
+    ~linkedlist() {
+        if(head) {
+            Ptr *curr = head, *next;
+            while(curr) {
                 next = curr->next;
                 delete curr;
-                curr = next;
+                head = curr = next;
             }
-
-            head = NULL;
         }
     }
 
-    Data& front()
-    {
+    Data& front() {
         return head->data;
     }
 
-    Data& back()
-    {
-        Ptr *curr = head;
-        Ptr *prev;
-
-        while(curr != NULL)
-        {
+    Data& back() {
+        Ptr *curr = head, *prev;
+        while(curr) {
             prev = curr;
             curr = curr->next;
         }
-
         return prev->data;
     }
 
-    void push_front(const Data& d)
-    {
-        Ptr *first = new Ptr;
-        first->data = d;
-        first->next = head;
-        head = first;
+    void push_front(const Data& d) {
+        head = new Ptr(d, nullptr);
     }
 
-    void push_back(const Data &d)
-    {
-        if(head == NULL)
-        {
-            Ptr *first = new Ptr;
-            first->data = d;
-            first->next = NULL;
-            head = first;
-        }
-        else
-        {
+    void push_back(const Data &d) {
+        if(!head) push_front(d);
+        else {
             Ptr *curr = head;
-
-            while(curr->next != NULL)
-            {
-                curr = curr->next;
-            }
-
-            Ptr *temp = new Ptr;
-            temp->data = d;
-            temp->next = NULL;
-
-            curr->next = temp;
+            while(curr->next) curr = curr->next;
+            curr->next = new Ptr(d, nullptr);
         }
     }
 
-    void push_sorted(const Data &d)
-    {
-        if(head == NULL)
-        {
-            Ptr *first = new Ptr;
-            first->data = d;
-            first->next = head;
-            head = first;
+    void push_sorted(const Data &d) {
+        if(!head) {
+            push_front(d);
             return;
         }
-        else
-        {
-            Ptr *curr = head;
-            Ptr *prev;
+        else {
+            Ptr *curr = head, *prev;
 
-            while(curr != NULL)
-            {
-                if(d < curr->data)
-                {
-                    break;
-                }
-
+            while(curr) {
+                if(d < curr->data) break;
                 prev = curr;
                 curr = curr->next;
             }
 
-            Ptr *toInsert = new Ptr;
-            toInsert->next = prev->next;
+            Ptr *toInsert = new Ptr(d, nullptr);
             prev->next = toInsert;
-            toInsert->data = d;
         }
     }
 
-    void pop_front()
-    {
-        if(head==NULL)
-        {
-            return;
-        }
-        else
-        {
+    void pop_front() {
+        if(!head) return;
+        else {
             Ptr *newHead = head->next;
             delete head;
             head = newHead;
         }
     }
 
-    void pop_back()
-    {
-        if(head == NULL)
-        {
-            return;
-        }
-        else
-        {
-            Ptr *curr = head;
-            Ptr *tail;
-
-            while(curr->next != NULL)
-            {
+    void pop_back() {
+        if(!head) return;
+        else {
+            Ptr *curr = head, *tail;
+            while(curr->next) {
                 tail = curr;
-                curr = curr->next;
+                tail->next = curr = curr->next;
             }
-
-            tail->next = NULL;
             delete curr;
         }
     }
 
-    void pop_at(const Data &d)
-    {
-        if(head == NULL)
-        {
-            return;
-        }
-        else if(front() == d)
-        {
-            pop_front();
-        }
-        else if(back() == d)
-        {
-            pop_back();
-        }
-        else
-        {
-            Ptr *curr = head;
-            Ptr *prev = NULL;
-
-            while(curr->next != NULL)
-            {
-                if(curr->data == d)
-                    break;
+    void pop_at(const Data &d) {
+        if(!head) return;
+        else if(front() == d) pop_front();
+        else if(back() == d) pop_back();
+        else {
+            Ptr *curr = head, *prev;
+            while(curr->next) {
+                if(curr->data == d) break;
                 prev = curr;
-                curr = curr->next;
+                prev->next = curr = curr->next;
             }
-
-            prev->next = curr->next;
-
             delete curr;
         }
     }
 
-    /** Print using cout **/
-    friend ostream& operator << (ostream& out, const linkedlist<Data> &ll)
-    {
-        if(ll.head == NULL)
-        {
-            return out;
-        }
-        else
-        {
+    friend ostream& operator << (ostream& out, const linkedlist<Data> &ll) {
+        if(ll.head == NULL) return out;
+        else {
             Ptr *curr = ll.head;
-
-            while(curr != NULL)
-            {
+            while(curr) {
                 out << curr->data << " ";
                 curr = curr->next;
             }
-
             return out;
         }
     }
